9f40516721c702e88c10e0fc9a4a727ba39580d8
PorevAJI6EPT <liubart.lojka@sollers.eu>
2022-04-14 12:03:56 +0200

    actions

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using StarChart.Data;
+using StarChart.Models;
 
 namespace StarChart.Controllers
 {
@@ -12,6 +16,47 @@ namespace StarChart.Controllers
         public CelestialObjectController(ApplicationDbContext context)
         {
             _context = context;
+        }
+
+        [HttpGet("{id:int}", Name = "GetById")]
+        public IActionResult GetById(int id)
+        {
+            var founded = _context.CelestialObjects.Find(id);
+            if (founded == null)
+            {
+                return NotFound();
+            }
+            founded.Satellites = GetSatellites(founded);
+            return Ok(founded);
+        }
+        [HttpGet("{name}", Name = "GetByName")]
+        public IActionResult GetByName(string name)
+        {
+            var founded = _context.CelestialObjects
+                .Where(c => c.Name == name)
+                .ToList();
+            if (founded.Count() == 0)
+            {
+                return NotFound();
+            }
+            founded.ForEach(c => c.Satellites = GetSatellites(c));
+            return Ok(founded);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var founded = _context.CelestialObjects
+                .ToList();
+            founded.ForEach(c => c.Satellites = GetSatellites(c));
+
+            return Ok(founded);
+        }
+
+        private List<CelestialObject> GetSatellites(CelestialObject parent)
+        {
+            return _context.CelestialObjects.Where(c => c.OrbitedObjectId == parent.Id).ToList();
+            
         }
     }
 }
