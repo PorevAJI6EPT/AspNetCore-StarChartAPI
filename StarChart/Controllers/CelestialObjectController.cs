369697021a91a57cf2fcac87904807f729eaeac8
PorevAJI6EPT <liubart.lojka@sollers.eu>
2022-04-14 12:21:47 +0200

    tey

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -58,5 +58,64 @@ namespace StarChart.Controllers
             return _context.CelestialObjects.Where(c => c.OrbitedObjectId == parent.Id).ToList();
             
         }
+
+        [HttpPost]
+        public IActionResult Create([FromBody]CelestialObject model)
+        {
+            _context.CelestialObjects.Add(model);
+            _context.SaveChanges();
+            return CreatedAtRoute("GetById", new {id=model.Id}, model);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, [FromBody] CelestialObject model)
+        {
+            var old = _context.CelestialObjects.Find(id);
+            if (old == null)
+            {
+                return NotFound();
+            }
+            old.OrbitalPeriod=old.OrbitalPeriod;
+            old.Name=model.Name;
+            old.OrbitedObjectId = model.OrbitedObjectId;
+            //_context.Update(old);
+            _context.SaveChanges();
+            return NoContent();
+        }
+
+        [HttpPatch("{id}/{name}")]
+        public IActionResult RenameObject(int id, string name)
+        {
+
+            var old = _context.CelestialObjects.Find(id);
+            if (old == null)
+            {
+                return NotFound();
+            }
+            old.Name = name;
+            _context.Update(old);
+            _context.SaveChanges();
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var old = _context.CelestialObjects.Find(id);
+            if (old == null)
+            {
+                return NotFound();
+            }
+
+            List<CelestialObject> forDeleting = 
+                _context.CelestialObjects
+                .Where(c=>c.OrbitedObjectId==id)
+                .ToList();
+            forDeleting.Add(old);
+
+            _context.RemoveRange(forDeleting);
+            _context.SaveChanges();
+            return NoContent();
+
+        }
     }
 }
